fe89ac5fb59b948ee3732ce2343dcd0bd2d3ecbb
mikelitu <mikelitubiomed@gmail.com>
2023-09-21 17:06:40 +0100

    add the catheter length controller

--- controller.py
+++ controller.py
@@ -1,10 +1,17 @@
 import Sofa.Core as SC
 
-class MoveSphere(SC.Controller):
+class ControlCatheter(SC.Controller):
     def __init__(self, *args, **kwargs):
         SC.Controller.__init__(self, *args, **kwargs)
-        self.sphere = kwargs["sphere"]
+        self.limit = 0.15
+        node = kwargs["node"]
+        self.beam = node["InstrumentCombined"]["DOFs"]
+        self.init_pos = self.beam["position"].value[-1][1]
+        self.instrument_controller = node["InstrumentCombined"]["m_ircontroller"]
     
     def onAnimateBeginEvent(self, event):
-        # print(self.sphere["position"].value)
+        pos = self.beam["position"].value[-1][1]
+        if pos > self.limit:
+            print("Catheter stopped!")
+            self.instrument_controller["speed"].value = 0.0
         pass
